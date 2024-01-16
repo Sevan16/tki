ab896a558d7e66574481075f469d5f7d3c8d49cb
Sevan16 <145327367+Sevan16@users.noreply.github.com>
2024-01-16 23:25:35 +0300

    3.2 переделал

--- main (14).c
+++ main (14).c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <math.h>
+#include <float.h>
 
 /**
 * @breaf проверяет правильность ввода данных
@@ -41,7 +42,17 @@ double series_with_e(const double e);
 * @param n - число для вычисления
 * @return Возвращает факториал числа
 */
-unsigned long long factorial (int n);
+
+/**
+ * @brief Проверка, что e не превышает модуля первого члена ряда (для k=1)
+ * @param e - заданное число e
+ */
+void check_e(double e);
+
+/**
+ * @brief Выводит сообщение об ошибке и завершает программу.
+ */
+void errors();
 
 /**
  * @brief Точка входа в программу.
@@ -54,6 +65,7 @@ int main()
     printf("The sum of series is: %lf", series(n));
     printf("\nInput the number of e: ");
     double e = double_get_value();
+    check_e(e);
     printf("Result: %lf", series_with_e(e));
     return 0;
 }
@@ -62,11 +74,9 @@ int int_get_value()
 {
     int result;
     int scanf_result = scanf("%d", &result);
-    if ((scanf_result!=1) || (result < 0))
+    if ((scanf_result!=1) || (result <= 0))
     {
-        errno = EIO;
-        perror("Error");
-        abort();
+        errors();
     }
     return result;
 }
@@ -75,10 +85,9 @@ double double_get_value()
 {
     double result;
     double scanf_result = scanf("%lf", &result);
-    if (scanf_result!=1 || result <= 0){
-        errno = EIO;
-        perror("Error");
-        abort();
+    if (scanf_result!=1)
+    {
+        errors();
     }
     return result;
 }
@@ -86,35 +95,57 @@ double double_get_value()
 double series(const int n)
 {
     double result = 0;
-    for (int k = 0; k <= n; k++)
+    for (int k = 1; k <= n; k++)
     {
         result += element(k);
     }
     return result;
 }
 
-unsigned long long factorial (int n)
-{
-    unsigned long long r;
-    for (r = 1; n > 1; r *= (n--))
-        ;
-    return r;
-}
-
 double element(const int k)
 {
-    return(pow(-1,k) * k)/ factorial(k);
+    double result;
+    if (k % 2 != 0)
+    {
+        result = -1;
+    }
+    else
+    {
+        result = 1;
+    }
+    for (int i = k-1; i > 1; i--)
+    {
+        double j = pow(i, -1);
+        result = result * j;
+    }
+    return result;
 }
+
 double series_with_e(const double e)
 {
-    int k = 2;
+    int k = 1;
     double sum = 0;
     double current_element = element(k);
-    while (current_element >= e)
+    while (fabs(current_element)-e >= -DBL_EPSILON)
     {
         current_element = element(k);
         sum += current_element;
-        k += 2;
+        k += 1;
     }
     return sum;
+}
+
+void check_e(double e)
+{
+    if (e >= fabs(element(1)))
+    {
+        errors();
+    }
+}
+
+void errors()
+{
+    errno = EIO;
+    perror("Error");
+    abort();
 }
